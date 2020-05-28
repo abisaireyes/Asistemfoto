40b9c96beb8ac7bf1f15717299126a1d9dccc41b
abisaireyes <abireyesglez@hotmail.es>
2020-05-27 22:26:43 -0600

    comit inicial con eclipse

---
+++ Asistemfoto/src/main/java/com/abisaireyes/asistemfoto/AsistemfotoApplication.java
@@ -0,0 +1,13 @@
+package com.abisaireyes.asistemfoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistemfotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistemfotoApplication.class, args);
+	}
+
+}
---
+++ Asistemfoto/src/test/java/com/abisaireyes/asistemfoto/AsistemfotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abisaireyes.asistemfoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistemfotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
